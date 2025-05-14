cffc58d7e184610fa338e4c64faa81892f8dbb81
GBZEIRAX <gabrielcorrea158@gmail.com>
2025-05-14 17:34:15 -0400

    projeto conta banco via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite sua conta !");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu Nome !");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite seu saldo !");
+        Double saldo = scanner.nextDouble();
+        
+
+        //imprimindo os dados obtidos pelo usuário
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +" , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
